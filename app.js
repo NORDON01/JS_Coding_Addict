9afaf4cc6a3206d7dc68a1e64a7e8a0206ebc6a3
nordon <georges.nordon@gmail.com>
2025-08-27 01:36:51 +0200

    Number challenge done

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-alert("Hello world 2");
-console.log("Hello world 4");
+let log = console.log;
+
+const score1 = 14;
+const score2 = 25;
+const score3 = 32;
+
+let total = score1 + score2 + score3;
+log("total: " + total + " average: " + total / 3);
+
+const plates = 20;
+const people = 7;
+let remaining = 20 % 7;
+
+remaining++;
+let message = "";
+message = "there are " + remaining + " plates";
+message;
