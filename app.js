8eba74460514f7125f3c9441de0759840cd41150
nordon <georges.nordon@gmail.com>
2025-08-28 14:26:19 +0200

    arrays, functions, objects

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const log = console.log;
 
-function greet(name) {
-  let yourName = name;
-  log("hello " + yourName);
-}
+const car = {
+  make: "Peugeot",
+  model: 604,
+  year: 2018,
+  colors: ["blue", "silver"],
+  hybrid: true,
+  drive: function () {
+    log("This car is runing fast");
+  },
+  stop: function () {
+    log("The car is parked");
+  },
+};
 
-greet("Paul");
-
-//OU
-
-let fname = "Peter";
-greet(fname);
+log(car.make);
+log(car.colors[0]);
+car.drive();
+car.stop();
