4c64cef63091474930261091eeca19a025e53228
NORDON01 <georges.nordon@gmail.com>
2025-09-04 20:16:24 +0200

    A jour jusquau Math object

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
 const log = console.log;    //global scope
 
-log(students);  
-//Ajout d'une propriété
-let newStudents = students.map(function(student){
-    student.role = 'student';
-    return student;
-})
+log(students);
 
-log (newStudents);
+let survey = students.reduce(function(survey, student){
+    let favSubject = student.favoriteSubject;
 
-//Sélection des meilleurs étudiants
-let highScores = students.filter(function(student){
-   return student.score >= 80;
-   //ou if(student.score >= 80){return student} 
-})
+    if(survey[favSubject]){survey[favSubject]++}
+    else{survey[favSubject] = 1};
 
-log(highScores);
+    return(survey)   
+}, {});
 
-//Sélection de l'étudiant avec l'id === 1
-let singleStudent = students.find(function(student){
-    return student.id === 1;
-})
-
-log (singleStudent);
-
-
-//Calcul du score moyen
-let averageScore = students.reduce(function (acc, student){
-    log(student.id);
-    log(`${acc}`);
-    acc += student.score;
-    return acc;
-},0)/students.length;
-
-log(`Average score: ${averageScore}`);
+log(survey);
