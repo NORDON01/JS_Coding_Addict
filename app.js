3788f83645e344f976f863f0baac35e2ed5931f8
NORDON01 <georges.nordon@gmail.com>
2025-09-06 22:39:12 +0200

    A jour jusqu'à navigating the DOM

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 const log = console.log;    //global scope
 
-const date = new Date();  //Possibilité d'utiliser une autre date 1/15/2005
- const moiss = ['Janvier', 'Février', 'Mars', 'Avril',
-                      'Mai','Juin', 'Juillet', 'Août',
-                      'Septembre','Octobre', 'Novembre', 'Décembre'];
-      const jours = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi',
-                    'Jeudi', 'Vendredi', 'Samedi',]
+const fruit = document.querySelector('.special');
+log(fruit);         //SÉLECTION DU 1er fruit de la classe ".special"  
 
-      let indexm = date.getMonth();
-      let mois = moiss[indexm]
-      log(mois);
-      
-      let indexj = date.getDay();
-      let joursem = jours[indexj];                                       
-      log(joursem);
+const fruits = document.querySelectorAll('.special');
+log(fruits);        //CECI EST UN ARRAY (une nodelist) => FOREACH est possible
 
-      let jourmoi = date.getDate();
-      log(jourmoi);  
-
-      let an = date.getFullYear();     
-      log(an);
-
-      let sentence = `${joursem}, ${jourmoi} ${mois} ${an}`;
-      log(sentence); 
-
-      document.body.innerHTML = sentence;
+fruits.forEach(function(fruit){
+    log(fruit);
+})
