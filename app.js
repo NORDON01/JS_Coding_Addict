df42fe97452a4b00627b64797219441665f535de
NORDON01 <georges.nordon@gmail.com>
2025-09-10 12:04:40 +0200

    A jour jusque class list / name

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const log = console.log;    //global scope
 
-const fruit = document.querySelector('.special');
-log(fruit);         //SÉLECTION DU 1er fruit de la classe ".special"  
+//GET ATTRIBUTE
+const first = document.querySelector('.first'); //Récupère 1er élément ".first"
+const classValue = first.getAttribute('id'); //1er attribut de type 'id'(class..)
+log(classValue);                             // valeur de l'attribut:"2emeAtt"
+//log(firstValue.textContent);               // text of class of first.
 
-const fruits = document.querySelectorAll('.special');
-log(fruits);        //CECI EST UN ARRAY (une nodelist) => FOREACH est possible
+const link = document.getElementById('link');
+const linkValue = link.getAttribute('href');
+log(linkValue);
 
-fruits.forEach(function(fruit){
-    log(fruit);
-})
+
+//SET ATTRIBUTE
+let allFirstElements = document.querySelectorAll('.first');
+log(allFirstElements);                       //NodeList
+
+const last = link.nextElementSibling;
+log(last);
+last.setAttribute('class', 'first');
+last.textContent = 'I now have a class attribute';
+log(last);
+
+allFirstElements = document.querySelectorAll('.first');
+log(allFirstElements);   
